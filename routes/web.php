8756e49ba9d2c81573ecbdb92b36ba63406c1d4c
YiJing0502 <w71080635@gmail.com>
2023-09-08 07:39:47 +0800

    資料更新

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+// 之後新增的
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\FontController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\ReplyController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +21,71 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+// 之後新增的
+Route::get('/front-index', [FontController::class, 'index'])->name('front-index');
+
+Route::prefix('/cart')->group(function () {
+
+    Route::get('/add', [CartController::class, 'create'])->name('cartAdd');
+    Route::post('/store', [CartController::class, ('store')])->name('cartStore');
+    Route::get('/product-list', [CartController::class, 'index'])->name('cartProductList');
+    Route::get('/edit/{id}', [CartController::class, 'edit'])->name('cartEdit');
+    Route::post('/update/{id}', [CartController::class, 'update'])->name('cartUpdate');
+    // 刪除
+    Route::post('/destroy/{id}', [CartController::class, 'destroy'])->name('cartDestroy');
+});
+// Route::get('test', [CartController::class, 'index']);
+Route::prefix('/type')->group(function () {
+    Route::get('/product-list', [TypeController::class, 'index'])->name('typeProductList');
+
+    Route::get('/add', [TypeController::class, 'create'])->name('typeAdd');
+    Route::post('/store', [TypeController::class, 'store'])->name('typeStore');
+
+    Route::get('/edit/{id}', [TypeController::class, 'edit'])->name('typeEdit');
+    Route::put('/update/{id}', [TypeController::class, 'update'])->name('typeUpdate');
+    // 刪除
+    Route::delete('/destroy/{id}', [TypeController::class, 'destroy'])->name('typeDestroy');
+});
+
+Route::prefix('/message')->group(function () {
+    Route::get('/index', [MessageController::class, 'index'])->name('messageIndex');
+
+    Route::post('/replayStore/{id}', [MessageController::class, 'replayStore'])->name('replayStore');
+    Route::post('/store', [MessageController::class, 'store'])->name('messageStore');
+
+    Route::get('/edit/{id}', [MessageController::class, 'edit'])->name('messageEdit');
+
+    Route::put('/replayUpdate/{id}', [MessageController::class, 'replayUpdate'])->name('replayUpdate');
+
+    Route::put('/update/{id}', [MessageController::class, 'update'])->name('messageUpdate');
+    // 刪除
+    Route::delete('/destroy/{id}', [MessageController::class, 'destroy'])->name('messageDestroy');
+
+    Route::delete('/reply/destroy/{id}', [MessageController::class, 'replayDestroy'])->name('replyDestroy');
+});
+
+Route::prefix('/reply')->group(function () {
+    Route::get('/index', [ReplyController::class, 'index'])->name('replyIndex');
+
+    Route::get('/add', [ReplyController::class, 'create'])->name('replyAdd');
+    Route::post('/store', [ReplyController::class, 'store'])->name('replyStore');
+
+    Route::get('/edit/{id}', [ReplyController::class, 'edit'])->name('replyEdit');
+    Route::put('/update/{id}', [ReplyController::class, 'update'])->name('replyUpdate');
+    // 刪除
+    // Route::delete('/reply/destroy/{id}', [ReplyController::class, 'destroy'])->name('replyDestroy');
 });
