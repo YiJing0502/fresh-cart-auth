3d1f40e0cf49e66a48188dae919f0744b4d85141
YiJing0502 <w71080635@gmail.com>
2023-09-13 16:35:15 +0800

    新增購物車相關Model與關聯

---
+++ app/Models/Cart.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+    protected $table = 'carts';
+    protected $primaryKey = 'id';
+    protected $fillable = ['created_at', 'updated_at', 'user_id','type_id', 'product_id', 'desire_qty'];
+    // 關聯 product_id 多對一
+    public function product() {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+    // 關聯 user_id 多對一
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,4 +11,8 @@ class Product extends Model
     protected $table = 'products';
     protected $primaryKey = 'id';
     protected $fillable = ['created_at', 'updated_at','name', 'img_path', 'price','status','descr'];
+    // 關聯 購物車：一對多
+    public function cart() {
+        return $this->hasMany(Cart::class, 'product_id', 'id');
+    }
 }
