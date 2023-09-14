8e791dc51acc9582a1614ef8fedeb308375b1f27
YiJing0502 <w71080635@gmail.com>
2023-09-14 08:26:36 +0800

    修改購物車頁面資訊

--- resources/views/front_end/shopprocess/orderdetails.blade.php
+++ resources/views/front_end/shopprocess/orderdetails.blade.php
@@ -31,24 +31,28 @@
                 </div>
                 <p class=" mb-5">already have account? Click here to <a href="">Sign in</a> .</p>
             </div>
-            <form class="mt-5 m-auto d-flex flex-column" action="{{ route('shopDeliverPost') }}" method="POST">
-                @csrf
+            <div class="mt-5 m-auto d-flex flex-column">
                 <ul class="border">
                     <div class="border-bottom p-2">Order details</div>
                     <div class="w-100">
+                        @foreach ($cart as $item)
+                        @dump($item->product)
                         <li class="card  w-100 d-flex flex-row align-items-center" style="width: 18rem;">
-                            <img src="{{ asset('image/kailiu2.jpg') }}" class="card-img-top w-25" alt="...">
+                            <img src="{{$item->product->img_path}}" class="card-img-top w-25" alt="...">
                             <div class="card-body d-flex flex-row flex-wrap">
                                 <div>
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up
-                                        the
-                                        bulk of
-                                        the
-                                        card's content.</p>
+                                    <h5 class="card-title">      {{$item->product->name}}
+                                    </h5>
+                                    <p class="card-text">
+                                        {{$item->product->descr}}
+                                    </p>
+                                    <p class="card-text">
+                                        <span>$</span>
+                                        {{$item->product->price}}
+                                    </p>
                                 </div>
                                 <div>
-                                    <a href="#" class="btn btn-primary m-1 mt-3">+</a>1
+                                    <a href="#" class="btn btn-primary m-1 mt-3">+</a>
                                     <a href="#" class="btn btn-primary m-1 mt-3">-</a>
                                 </div>
                             </div>
@@ -56,38 +60,21 @@
                                 $15
                             </div>
                         </li>
-                        <li class="card w-100  d-flex flex-row align-items-center" style="width: 18rem;">
-                            <img src="{{ asset('image/kailiu3.jpg') }}" class="card-img-top w-25" alt="...">
-                            <div class="card-body d-flex flex-row flex-wrap">
-                                <div>
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up
-                                        the
-                                        bulk of
-                                        the
-                                        card's content.</p>
-                                </div>
-                                <div>
-                                    <a href="#" class="btn btn-primary m-1 mt-3">+</a>1
-                                    <a href="#" class="btn btn-primary m-1 mt-3">-</a>
-                                </div>
-                            </div>
-                            <div class="me-5">
-                                $15
-                            </div>
-                        </li>
+                        @endforeach
+                        {{-- 總金額 --}}
                         <div class="w-100 d-flex flex-row justify-content-between border-bottom">
                             <div class="p-2">subtotal</div>
                             <div class="p-2">$30</div>
                         </div>
                     </div>
+                    {{-- 下一步 --}}
                     <div class=" w-100 d-flex justify-content-end">
                         <button type="submit" class="btn btn-primary align-self-end mt-2 p-2">
                             下一步
                         </button>
                     </div>
                 </ul>
-            </form>
+            </div>
         </div>
     </div>
 @endsection
